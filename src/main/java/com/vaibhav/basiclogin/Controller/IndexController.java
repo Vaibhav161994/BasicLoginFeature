75565afccc40d6d8daea28452a7db00fb37c9f7c
kaushv5 <vaibhav.kaushik1694@gmail.com>
2018-07-07 21:00:25 +0530

    Test'

--- src/main/java/com/vaibhav/basiclogin/Controller/IndexController.java
+++ src/main/java/com/vaibhav/basiclogin/Controller/IndexController.java
@@ -3,13 +3,19 @@ package com.vaibhav.basiclogin.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by kaushv5 on 6/29/2018.
  */
 @Controller
+//@RequestMapping("/api")
 public class IndexController {
 
+    @RequestMapping("hello")
+    public String hello(){
+        return "Hello World";
+    }
     @RequestMapping(path = {"/login","/"}, method = RequestMethod.GET)
     public String LoginPage(){
         return "login";
